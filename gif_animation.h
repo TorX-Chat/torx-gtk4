c0c62a05e3440f89f93d648e6b58f00e9a6e34e9
TorX.Chat <torx@torx.chat>
2024-11-05 08:01:55 +0800

    In honor of Guy Fawkes and Guy Fawkes Day, this software has been GPL'd with section 7 exemptions.

--- gif_animation.h
+++ gif_animation.h
@@ -1,3 +1,31 @@
+/*
+TorX: Metadata-safe Tor Chat Library 
+Copyright (C) 2024 TorX
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as
+published by the Free Software Foundation.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Appendix:
+
+Section 7 Exceptions:
+1) Modified versions of the material and resulting works must be clearly titled in the following manner: "Unofficial TorX by Financier", where the word Financier is replaced by the financier of the modifications. Where there is no financier, the word Financier shall be replaced by the organization or individual who is primarily responsible for causing the modifications. Example: "Unofficial TorX by The United States Department of Defense". This amended full-title must replace the word "TorX" in all source code files and all resulting works. Where utilizing spaces is not possible, underscores may be utilized. Example: "Unofficial_TorX_by_The_United_States_Department_of_Defense". The title must not be replaced by an acronym or short title in any form of distribution.
+
+2) Modified versions of the material and resulting works must be distributed with alternate logos and imagery that is substantially different from the original TorX logo and imagery, especially the 7-headed snake logo. Modified material and resulting works, where distributed with a logo or imagery, should choose and distribute a logo or imagery that reflects the Financier, organization, or individual primarily responsible for causing modifications and must not cause any user to note similarities with any of the original TorX imagery. Example: Modifications or works financed by The United States Department of Defense should choose a logo and imagery similar to existing logos and imagery utilized by The United States Department of Defense.
+
+3) Those who modify, distribute, or finance the modification or distribution of the material or resulting works, shall not avail themselves of any disclaimers of liability, such as those laid out by the original TorX author in sections 15 and 16 of the License.
+
+4) Those who modify, distribute, or finance the modification or distribution of the material or resulting works, shall jointly and severally indemnify the original TorX author against any claims of damages incurred and any costs arising from litigation related to any changes they are have made, caused to be made, or financed. 
+
+5) The original author of TorX may issue explicit exemptions from the above requirements (Such as, for example, necessary changes for package maintenance in official Debian repositories), but such exemptions should be interpretted in the narrowest possible scope and to only grant limited rights within the narrowest possible scope to those who explicitly receive the exemption and not those who receive the material or resulting works from the exemptee.
+
+6) The original author of TorX grants no exceptions from trademark protection in any form.
+
+7) Each aspect of these exemptions are to be considered independent and severable if found in contradiction with the License or applicable law.
+*/
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
